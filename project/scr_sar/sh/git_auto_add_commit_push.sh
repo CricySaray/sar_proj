2638623d3b242fa2af9e13bd2efd4840faa8295c
cricy <cricysaray@qq.com>
2025-07-09 16:05:02 +0800

    Auto commit at 2025-07-09 16:05

--- project/scr_sar/sh/git_auto_add_commit_push.sh
+++ project/scr_sar/sh/git_auto_add_commit_push.sh
@@ -7,39 +7,39 @@ LOG_FILE="/home/cricy/log/git_auto_commit.log"
 cd "$REPO_PATH" || exit
 DATE="[$(date)]"
 if [[ -n $(git ls-files --others --exclude-standard) ]]; then
-  echo "$(date) : have untracked files, begin add to stage" >> "$LOG_FILE"
+  echo "$DATE : have untracked files, begin add to stage" >> "$LOG_FILE"
   git status -s >> "$LOG_FILE"
   git add --all >> "$LOG_FILE"
 else
-  echo "$(date) : DONT find untracked files, continue..." >> "$LOG_FILE"
+  echo "$DATE : DONT find untracked files, continue..." >> "$LOG_FILE"
 fi
 
 # 检查是否有未提交的变更
 if ! git diff --quiet || ! git diff --cached --quiet; then
     # 记录日志
-    echo "$(date) : monitor modified files, beginning to add and commit ..." >> "$LOG_FILE"
-    echo "$(date) : below is files to add:" >> "$LOG_FILE"
+    echo "$DATE : monitor modified files, beginning to add and commit ..." >> "$LOG_FILE"
+    echo "$DATE : below is files to add:" >> "$LOG_FILE"
     git status -s >> "$LOG_FILE"
 
     # 添加所有变更
-    echo "$(date) : begin to add --all: " >> "$LOG_FILE"
+    echo "$DATE : begin to add --all: " >> "$LOG_FILE"
     git add --all >> "$LOG_FILE"
     
     # 提交变更
-    echo "$(date) : begin to commit:" >> "$LOG_FILE"
+    echo "$DATE : begin to commit:" >> "$LOG_FILE"
     git commit -m "Auto commit at $(date +'%Y-%m-%d %H:%M')" >> "$LOG_FILE"
     
     # 推送变更
-    echo "$(date) : begin to push to proj/main" >> "$LOG_FILE"
+    echo "$DATE : begin to push to proj/main" >> "$LOG_FILE"
     git push --set-upstream proj main >> "$LOG_FILE"
     
     # 记录成功日志
-    echo "$(date) : commit and push successfully" >> "$LOG_FILE"
-    echo "$(date) : ----------------------------------" >> "$LOG_FILE"
+    echo "$DATE : commit and push successfully" >> "$LOG_FILE"
+    echo "$DATE : ----------------------------------" >> "$LOG_FILE"
     echo " " >> "$LOG_FILE"
 else
     # 记录无变更日志
-    echo "$(date) : No changed files detected" >> "$LOG_FILE"
-    echo "$(date) : ----------------------------------" >> "$LOG_FILE"
+    echo "$DATE : No changed files detected" >> "$LOG_FILE"
+    echo "$DATE : ----------------------------------" >> "$LOG_FILE"
     echo " " >> "$LOG_FILE"
 fi

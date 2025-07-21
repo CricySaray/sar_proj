2147f11eecc60fe0b217a2766d8f7581122d02f8
cricy <cricysaray@qq.com>
2025-07-21 17:30:31 +0800

    auto: normal update

--- project/ref_from_others/test_for_search/8810flow_additional/ict8810/common_script/disable_boundary_dffs_useful_skew.tcl
+++
@@ -1,7 +0,0 @@
-set boundary_in_ffs [get_db [get_db [get_db [get_db [get_db [all_fanout -from [get_db [get_db ports -if {.direction == "in" && .name != "ijtag_reset" && .name != "ijtag_se"  && .name != "ict_scan_se"}] .name] -endpoints_only ] -if {.obj_type == pin && .is_data == true && .is_clear == false && .is_async == false && .is_clock == false && .base_pin.base_cell.class == core}] -if {.inst.base_cell.is_latch == false && .inst.base_cell.is_sequential == true && .inst.base_cell.is_integrated_clock_gating == false}] .inst] -unique .pins -if {.is_clock == true}] .name]
-set boundary_out_ffs [get_db [get_db [all_fanin -to [get_db [get_db ports -if {.direction == "out"}] .name] -startpoints_only ] -if {.obj_type == pin && .inst.is_sequential == true && .base_pin.base_cell.class == core}] .name]
-set boundary_ffs  [concat $boundary_in_ffs $boundary_out_ffs]
-
-foreach pins $boundary_ffs {
-    set_ccopt_property schedule off -pin $pins
-}
--- project/ref_from_others/test_for_search/8810flow_additional/ict8810/sta_flow/scr/write_sdf.tcl
+++
@@ -1,7 +0,0 @@
-set current_dir [pwd]
-set DESIGN_NAME lb_cpu_top
-set mode        scan
-set corner      typ_85
-set check       setuphold;#setup hold
-set VIEW        eco_39
-write_sdf -version  3.0 -no_edge -context verilog -significant_digits 5 -compress gzip -include  "SETUPHOLD RECREM" -exclude { no_condelse clock_tree_path_models } -no_negative_values timing_checks  -no_internal_pins    ${current_dir}/../../dsn/sdf/${DESIGN_NAME}.${mode}_${corner}_${check}.${VIEW}.sdf

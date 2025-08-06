5086d76784ff97f882a4835f1c3b2698d325083b
cricy <cricysaray@qq.com>
2025-08-06 18:46:20 +0800

    auto: normal update

--- project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_judgeIfLoop_forOne2More.invs.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_judgeIfLoop_forOne2More.invs.tcl
@@ -7,7 +7,9 @@
 # descrip   : judge if it has loop problem using Algorithm DBSCAN(proc cluster_points)
 # test entry: you can test all nets ratio of all design using ../../../scr_sar/tcl/packages/testAllNetRatio_using_cluster_point.test.tcl
 #             and then specify the threshold that is been loopped!
-# return    : 
+# return    : 0|1|2|3
+#             0: no loop
+#             1|2|3: have loop problem
 # ref       : link url
 # --------------------------
 source ./proc_ifInBoxes.invs.tcl; # ifInBoxes
@@ -33,8 +35,8 @@ proc judgeIfLoop_forOne2More {{driverPinPt {}} {sinksPinPt {}} {netLength 0} {ch
     set ratio [expr $netLength / $ruleNetLength]
     switch $checkMode {
       "strict" { set threshold {1.4 1.8 2.3} } 
-      "relax"  { set threshold {} }
       "normal" { set threshold {1.7 2.3 3} } 
+      "relax"  { set threshold {2.3 3.2 4} }
     }
     return $ratio
   }

c68ffe87ce4640511e5ef21fc4ef304a7cd3ff4f
cricy <cricysaray@qq.com>
2025-08-07 18:10:01 +0800

    Auto commit at 2025-08-07 18:10

--- project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_getAllInfo_fromPin.invs.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_getAllInfo_fromPin.invs.tcl
@@ -96,12 +96,12 @@ proc get_allInfo_fromPin {{pinname ""} {forbidenVT {AH9}} {driveCapacityRange {1
     dict set allInfo ifNetConnected [judge_ifAllSegmentsConnected [dict get $allInfo wiresPts]] ; # 1: connected 0: not connected
 
     dict set allInfo ruleLen [eo [expr [dict get $allInfo numSinks] == 1] [lindex $resultOfIfLoop_forOne2More end] [dict get $allInfo distanceOfDriver2CenterOfSinksPinPt]]
-    dict set allInfo sink_pt_D2List [lmap sinkpinname [dict get $allInfo sinksPin { set sinkpt [gpt $sinkpinname] ; set sink_pt [list $sinkpinname $sinkpt] }]]
+    dict set allInfo sink_pt_D2List [lmap sinkpinname [dict get $allInfo sinksPin] { set sinkpt [gpt $sinkpinname] ; set sink_pt [list $sinkpinname $sinkpt] }]
     dict set allInfo sinkPinFarthestToDriverPin [lindex [find_farthest_sinkpoint_to_driver_pin [dict get $allInfo driverPinPT] [dict get $allInfo sink_pt_D2List]]] 
     dict set allInfo sinksCellClassForShow [eo [expr [dict get $allInfo numSinks] == 1] [dict get $allInfo sinksCellClass] [dict get $allInfo uniqueShortenedSinksCellClass]]
     dict set allInfo farthestSinkCellType [dbget [dbget top.insts.instTerms.name [dict get $allInfo sinkPinFarthestToDriverPin] -p2].cell.name]
     
-    dict foreach {key val} $allInfo  { set $key $val }
+    dict for {key val} $allInfo  { set $key $val }
     dict set allInfo infoToShow [list $netLen $ruleLen $ifLoop $driverCellClass $driverCellType $driverPin $numSinks $sinksCellClassForShow $farthestSinkCellType $sinkPinFarthestToDriverPin]
     return $allInfo
   }

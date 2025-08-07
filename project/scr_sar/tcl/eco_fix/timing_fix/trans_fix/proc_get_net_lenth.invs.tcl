f334b80cf0aa83513ef159dc307eabeba7060c96
cricy <cricysaray@qq.com>
2025-08-08 01:37:01 +0800

    update test_mux_forAllOutputPin.tcl

--- project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_get_net_lenth.invs.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_get_net_lenth.invs.tcl
@@ -6,18 +6,21 @@
 #   -> (atomic_proc|display_proc)
 # descrip   : get net length. ONLY one net!!!
 # update    : 2025/07/27 22:32:07 Sunday
-#             fix bug: for net: {{xxx/xxx/xxx[12]}}, it can be converted to {xxx/xxx/xxx[12]}
+#             (U001) fix bug: for net: {{xxx/xxx/xxx[12]}}, it can be converted to {xxx/xxx/xxx[12]}
+# update    : 2025/08/08 01:30:09 Friday
+#             (U002) fix bug when wires segments not exist, have no physical net
 # ref       : link url
 # --------------------------
 alias gl "get_net_length"
 proc get_net_length {{net ""}} {
-  if {[lindex $net 0] == [lindex $net 0 0]} {
+  if {[lindex $net 0] == [lindex $net 0 0]} { ; # U001
     set net [lindex $net 0]
   }
 	if {$net == "0x0" || [dbget top.nets.name $net -e] == ""} { 
 		return "0x0:1"
 	} else {
-    set wires_split_length [dbget [dbget top.nets.name $net -u -p].wires.length]
+    set wires_split_length [dbget [dbget top.nets.name $net -u -p].wires.length -e]
+    if {$wires_split_length == ""} { return 0 } ; # U002
     set net_length 0
     foreach wire_len $wires_split_length {
       set net_length [expr $net_length + $wire_len]
--- project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/test_mux_forAllOutputPin.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/test_mux_forAllOutputPin.tcl
@@ -1,10 +1,14 @@
 source ./proc_mux_of_strategies.invs.tcl; # mux_of_strategies
 source ../../../packages/generate_randomNumber_withNormalDistribution.package.tcl; # generate_randomNumber_withNormalDistribution
 source ../../../packages/print_formattedTable.package.tcl; # print_formattedTable
+source ../../../eco_fix/timing_fix/trans_fix/proc_get_net_lenth.invs.tcl; # 
 proc test_all_output_pin {{sumFile "testPinOutput.list"}} {
   set allOutputPin [dbget [dbget top.insts.instTerms {.isOutput == 1}].name]
   set fo [open $sumFile w]
   foreach testpin $allOutputPin {
+    set netname [dbget [dbget top.insts.instTerms.name $testpin .p2].net.name ]
+    set netLen [get_net_length $netname]
+    if {!$netLen} {continue}
     set randomViolValue [generate_randomNumber_withNormalDistribution]
     puts "point [incr i]: viol: $randomViolValue | pin: $testpin"
     set dict_of_sum [mux_of_strategies $randomViolValue $testpin]

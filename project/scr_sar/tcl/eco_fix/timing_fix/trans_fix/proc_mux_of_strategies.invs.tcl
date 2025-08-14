a2d076876302e331032c9f19666ccac8ac36c535
cricy <cricysaray@qq.com>
2025-08-14 18:45:07 +0800

    Auto commit at 2025-08-14 18:45

--- project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_mux_of_strategies.invs.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_mux_of_strategies.invs.tcl
@@ -45,7 +45,7 @@ source ./proc_getAllInfo_fromPin.invs.tcl; # get_allInfo_fromPin
 #               ifHaveBeenLargestCapacityInRange/ifNetConnected/ruleLen/sink_pt_D2List/sinkPinFarthestToDriverPin/sinksCellClassForShow/farthestSinkCellType/
 #               [one2more: numFartherGroupSinks/fartherGroupSinksPin]/infoToShow
 alias mux_of_strategies "sliding_rheostat_of_strategies"
-proc sliding_rheostat_of_strategies {{violValue 0} {violPin ""} {VTweight {{SVT 3} {LVT 1} {ULVT 0}}} {ifCanChangeVTWhenChangeCapacity 1} {ifCanChangeVTcapacityWhenAddRepeater 1} {debug 0} {promptPrefix "# song"}} {
+proc sliding_rheostat_of_strategies {{violValue 0} {violPin ""} {VTweight {{AR9 3} {AL9 1} {AH9 0}}} {ifCanChangeVTWhenChangeCapacity 1} {ifCanChangeVTcapacityWhenAddRepeater 1} {debug 0} {promptPrefix "# song"}} {
   if {![string is double $violValue] || [expr $violValue > 0] || $violPin == "" || $violPin == "0x0" || [dbget top.insts.instTerms.name $violPin -e] == ""} {
     error "proc mux_of_strategies: check your input, violValue($violValue) is not double number or greater than 0 or violPin($violPin) is not found!!!"
   } else {

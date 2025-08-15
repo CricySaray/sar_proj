7b006e54964c56251c8417ad54b0f839f69142c8
cricy <cricysaray@qq.com>
2025-08-15 18:05:02 +0800

    Auto commit at 2025-08-15 18:05

---
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_getRect_innerAreaEnclosedByEndcap.invsGUI.tcl
@@ -0,0 +1,10 @@
+#!/bin/tclsh
+# --------------------------
+# author    : sar song
+# date      : 2025/08/15 17:59:32 Friday
+# label     : gui_proc
+#   -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc|misc_proc)
+# descrip   : get rects of inner area that is enclosed by boundary cell(endcap cell)
+# return    : rects
+# ref       : link url
+# --------------------------

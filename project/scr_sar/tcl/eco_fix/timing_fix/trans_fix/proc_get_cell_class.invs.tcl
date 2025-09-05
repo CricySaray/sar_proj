4afe691ec6a1d9ad8fd895c29f72dc2598c56e45
cricy <cricysaray@qq.com>
2025-09-05 18:25:06 +0800

    Auto commit at 2025-09-05 18:25

--- project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_get_cell_class.invs.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/trans_fix/proc_get_cell_class.invs.tcl
@@ -44,7 +44,7 @@ proc expandMapList {{process {}}} {
   } elseif {$process in {TSMC_cln40lp}} {
     set customMapList {} 
   } elseif {$process in {TSMC_tcbn40lpbwp}} {
-    set customMapList {{ANT {{ANTENNA*}}} {noCare {{BHD*} {^LVL*}}} {decap {{^G?DCAP*}}} {filler {{^G?FILL*}}} {IOfiller {{PFILL*}}} {cutCell {{^PBRK*}}} {padCell {{*_DR$}}} {ISOcell {{^ISO*}}} {tapCell {{^TAP*}}} {tieCell {{^TIE*}}}} 
+    set customMapList {{ANT {{ANTENNA.*}}} {noCare {{BHD.*} {^LVL.*}}} {decap {{^G?DCAP.*}}} {filler {{^G?FILL.*}}} {IOfiller {{PFILL.*}}} {cutCell {{^PBRK.*}}} {padCell {{.*_DR$}}} {ISOcell {{^ISO.*}}} {tapCell {{^TAP.*}}} {tieCell {{^TIE.*}}}} 
   }
   if {[llength $customMapList]} {
     set expandedMapList [lmap item_map $customMapList {

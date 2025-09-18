7e8505ef2e24924097767949322bc051eae5a079
cricy <cricysaray@qq.com>
2025-09-18 18:00:01 +0800

    Auto commit at 2025-09-18 18:00

---
+++ project/scr_sar/tcl/flow_build/runCmd_addFiller.invs.tcl
@@ -0,0 +1,11 @@
+#!/bin/tclsh
+# --------------------------
+# author    : sar song
+# date      : 2025/09/18 16:31:13 Thursday
+# label     : flow_proc
+#   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc|flow_proc|report_proc|misc_proc)
+#   perl -> (format_sub)
+# descrip   : what?
+# return    : run cmd of addFiller
+# ref       : link url
+# --------------------------

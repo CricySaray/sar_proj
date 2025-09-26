0371303c578522113c901dd2a88aac5c83f788b5
cricy <cricysaray@qq.com>
2025-09-27 00:14:50 +0800

    auto: normal update

--- .pt_alias.tcl
+++ .pt_alias.tcl
@@ -4,8 +4,6 @@
 alias sa "source ~/.pt_alias.tcl"
 alias rs "restore_session"
 alias sc "set_host_options -max_cores" ; # you need input number of max cores
-alias rt "report_timing -nos -input -nets -delay_type max -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3"
-alias rth "report_timing -nos -input -nets -delay_type min -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3"
-
-alias rts "report_timing -nos -input -nets -delay_type max -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3 -path_type summary"
-alias rtsh "report_timing -nos -input -nets -delay_type min -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3 -path_type summary"
+set common_options "-nos -input -nets -transition_time -variation -capacitance -crosstalk_delta -derate -significant_digits 3 -delay_type"
+alias rt "report_timing $common_options max -nworst 3"
+alias rth "report_timing $common_options min -nworst 3"

c4e2ab0cbd5859bc49162ff5f99e44d51816a8f0
cricy <cricysaray@qq.com>
2025-09-27 00:11:17 +0800

    auto: normal update

--- .pt_alias.tcl
+++ .pt_alias.tcl
@@ -1,10 +1,11 @@
 # ------------------------
 # pt alias 
 
+alias sa "source ~/.pt_alias.tcl"
 alias rs "restore_session"
 alias sc "set_host_options -max_cores" ; # you need input number of max cores
-alias rt "report_timing -nos -input -nets -delay_type max -transition_timing -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3"
-alias rth "report_timing -nos -input -nets -delay_type min -transition_timing -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3"
+alias rt "report_timing -nos -input -nets -delay_type max -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3"
+alias rth "report_timing -nos -input -nets -delay_type min -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3"
 
-alias rts "report_timing -nos -input -nets -delay_type max -transition_timing -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3 -path_type summary"
-alias rtsh "report_timing -nos -input -nets -delay_type min -transition_timing -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3 -path_type summary"
+alias rts "report_timing -nos -input -nets -delay_type max -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3 -path_type summary"
+alias rtsh "report_timing -nos -input -nets -delay_type min -transition_time -capacitance -crosstalk_delta -derate -significant_digits 3 -nworst 3 -path_type summary"

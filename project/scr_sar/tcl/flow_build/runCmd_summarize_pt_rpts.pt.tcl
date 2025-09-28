5a54887ce42267c1a41bf35735a4b1ae1241f2cc
cricy <cricysaray@qq.com>
2025-09-28 21:00:08 +0800

    Auto commit at 2025-09-28 21:00

--- project/scr_sar/tcl/flow_build/runCmd_summarize_pt_rpts.pt.tcl
+++ project/scr_sar/tcl/flow_build/runCmd_summarize_pt_rpts.pt.tcl
@@ -32,14 +32,19 @@ proc runCmd_summarize_pt_rpts {args} {
     regsub -- "-" $arg "" var
     set $var $opt($arg)
   }
-  set optionsOfFormatOfScenarios [list "<mode>" "<type>" "<voltage>" "<rcCorner>" "<temperature>"] ; # you only select options inside these
-  set mode $modesOfFormatExp ; set type $typeToCheckOfFormatExp ; set voltage $voltageOfFormatExp ; set rcCorner $rcCornerOfFormatExp ; set temperature $temperatureOfFormatExp
-  set allCasesOfScenarios [generate_combinations -connector "_" {*}[lmap temp_var [regsub {>|<} $optionsOfFormatOfScenarios ""] { if {![llength [subst \${$temp_var}]]} {error "proc runCmd_summarize_pt_rpts: check your input: the $temp_var is empty!!!"} else {set $temp_var}}]]
+  if {$scenarios == "auto"} {
+    set optionsOfFormatOfScenarios [list "<mode>" "<type>" "<voltage>" "<rcCorner>" "<temperature>"] ; # you only select options inside these
+    set mode $modesOfFormatExp ; set type $typeToCheckOfFormatExp ; set voltage $voltageOfFormatExp ; set rcCorner $rcCornerOfFormatExp ; set temperature $temperatureOfFormatExp
+    set allCasesOfScenarios [generate_combinations -connector "_" {*}[lmap temp_var [regsub {>|<} $optionsOfFormatOfScenarios ""] { if {![llength [subst \${$temp_var}]]} {error "proc runCmd_summarize_pt_rpts: check your input: the $temp_var is empty!!!"} else {set $temp_var}}]]
+  } else {
+    set allCasesOfScenarios $scenarios 
+  }
   if {![file isdirectory $searchDir]} {
     error "proc runCmd_summarize_pt_rpts: check your input: searchDir($searchDir) does not exist!!!" 
   }
   set allDirNameOnSearchDir [lmap temp_path [glob -nocomplaion $searchDir/*] { file tail $temp_path }]
   set scenarioDirs [lmap temp_dirname $allDirNameOnSearchDir { if {$temp_dirname in $allCasesOfScenarios} {set temp_dirname} else {continue} }]
+  if {$scenarioDirs == ""} { error "proc runCmd_summarize_pt_rpts: " }
   set allConstraintNameList [list min_delay max_daley max_capacitance max_transition max_fanout min_pulse_width min_period]
 
   set infoOfAllScenarios [lmap temp_scenario_dir $scenarioDirs {
@@ -61,9 +66,9 @@ proc runCmd_summarize_pt_rpts {args} {
     set reg2regTNS [lindex $valueList [lsearch -exact $titleList "reg2reg_TNS"]]
     set reg2regNUM [lindex $valueList [lsearch -exact $titleList "reg2reg_NUM"]]
     if {$typeOfScenario == "setup"} {set wns_type_delay $wns_max_delay ; set num_type_delay $num_max_delay} elseif {$typeOfScenario == "hold"} {set wns_type_delay $wns_max_delay ; set num_type_delay $num_max_delay} else {error "proc runCmd_summarize_pt_rpts: check your formatOfScenarios($formatOfScenarios), not find 'setup' or 'hold' !!!"}
-    list $wns_type_delay $num_type_delay $totalTNS $reg2regWNS $reg2regNUM $reg2regTNS $wns_max_transition $num_max_transition $wns_max_fanout $num_max_fanout $wns_max_capacitance $num_max_capacitance $wns_min_period $num_min_period $wns_min_pulse_width $num_min_pulse_width
+    list $temp_scenario_dir $wns_type_delay $num_type_delay $totalTNS $reg2regWNS $reg2regNUM $reg2regTNS $wns_max_transition $num_max_transition $wns_max_fanout $num_max_fanout $wns_max_capacitance $num_max_capacitance $wns_min_period $num_min_period $wns_min_pulse_width $num_min_pulse_width
   }]
-  set infoOfAllScenarios [linsert $infoOfAllScenarios 0 [list wns num tns r2r_w r2r_n r2r_t transW transN maxfanW maxfanN maxCapW maxCapN minPeriodW minPeriodN minPulseW minPulseN]]
+  set infoOfAllScenarios [linsert $infoOfAllScenarios 0 [list scenario wns num tns r2r_w r2r_n r2r_t transW transN maxfanW maxfanN maxCapW maxCapN minPeriodW minPeriodN minPulseW minPulseN]]
   set tableToDisplay [join [table_format_with_title $infoOfAllScenarios 0 ""]]
   if {$ifPrintToWindow} { puts $tableToDisplay }
   set fo [open "$searchDir/$outputFileOfSummary" w]
@@ -71,10 +76,24 @@ proc runCmd_summarize_pt_rpts {args} {
   close $fo
 }
 
-define_proc_arguments runCmd_summarize_pt_rpts \
+define_proc_attribute runCmd_summarize_pt_rpts \
   -info "run cmd of summarizing pt rpts"\
+  set searchDir                   "./"
+  set ifPrintToWindow             1
+  set outputFileOfSummary         "sum.rpt"
+  set scenarios                   "auto" ; # auto (will search ) or [list ...]
+  set formatOfScenarios           "<mode>_<type>_<voltage>_<rcCorner>_<temperature>" ; # func_setup_0p99v_rcworst_m40c
+  set modesOfFormatExp            {func scan}
+  set typeToCheckOfFormatExp      {setup hold}
+  set voltageOfFormatExp          {0p99v 1p1v 1p21v}
+  set rcCornerOfFormatExp         {cworst cbest rcworst rcbest typical}
+  set temperatureOfFormatExp      {25c m40c 125c}
+  set designName                  "SC5019_TOP" ; # only consisted of reportConstraintFileName
+  set reportConstraintFileName    "report_constraint_$designName.rpt"
+  set globalTimingCsvFileName     "${designName}.global_timing.csv" ; # please using command: report_global_timing -format csv -ouput .../$designName.global_timing.csv
   -define_args {
-    {-type "specify the type of eco" oneOfString one_of_string {required value_type {values {change add delRepeater delNet move}}}}
-    {-inst "specify inst to eco when type is add/delete" AString string require}
-    {-distance "specify the distance of movement of inst when type is 'move'" AFloat float optional}
+    {-searchDir "specify the dir to search" AString string optional}
+    {-ifPrintToWindow "if print to window" oneOfString one_of_string {optional value_help {values {0 1}}}}
+    {-outputFileOfSummary "specify the output file of summary" AString string optional}
+    {-scenarios ""}
   }

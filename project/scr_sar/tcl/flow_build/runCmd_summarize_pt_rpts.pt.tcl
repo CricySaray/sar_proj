3dff16aefe5ce0b6c57c50df61b14f0ed9ed588b
cricy <cricysaray@qq.com>
2025-09-28 21:50:08 +0800

    Auto commit at 2025-09-28 21:50

--- project/scr_sar/tcl/flow_build/runCmd_summarize_pt_rpts.pt.tcl
+++ project/scr_sar/tcl/flow_build/runCmd_summarize_pt_rpts.pt.tcl
@@ -15,7 +15,7 @@ source ./common/parse_constraint_report.common.tcl; # parse_constraint_report
 source ../packages/table_format_with_title.package.tcl; # table_format_with_title
 proc runCmd_summarize_pt_rpts {args} {
   set searchDir                   "./"
-  set outputFileOfSummary         "sum.csv"
+  set outputFileOfSummary         "$searchDir/<scenario>/sum.csv" ; # if have '<scenario>' in this path, this will write output for every scenario dir
   set scenarios                   "auto" ; # auto (will search ) or [list ...]
   set formatOfScenarios           "<mode>_<type>_<voltage>_<rcCorner>_<temperature>" ; # func_setup_0p99v_rcworst_m40c
   set modesOfFormatExp            {func scan}
@@ -68,8 +68,17 @@ proc runCmd_summarize_pt_rpts {args} {
     list $temp_scenario_dir $wns_type_delay $num_type_delay $totalTNS $reg2regWNS $reg2regNUM $reg2regTNS $wns_max_transition $num_max_transition $wns_max_fanout $num_max_fanout $wns_max_capacitance $num_max_capacitance $wns_min_period $num_min_period $wns_min_pulse_width $num_min_pulse_width
   }]
   set infoOfAllScenarios [linsert $infoOfAllScenarios 0 [list scenario wns num tns r2r_w r2r_n r2r_t transW transN maxfanW maxfanN maxCapW maxCapN minPeriodW minPeriodN minPulseW minPulseN]]
-  set tableToDisplay [join [table_format_with_title $infoOfAllScenarios 0 ]]
-  set fo [open "$searchDir/$outputFileOfSummary" w]
+  set tableToDisplay [join [table_format_with_title $infoOfAllScenarios 0]]
+  if {[regexp {<scenario>} $outputFileOfSummary]} { 
+    if {$scenarios == "auto"} { 
+      set allOutputFileOfSummary [lmap temp_scenario $allCasesOfScenarios { 
+        set temp_output_file_of_summary [string map [list <scenario> $temp_scenario]]
+      }]
+    }
+  } else {
+    set allOutputFileOfSummary []
+  }
+  set fo [open "$outputFileOfSummary" w]
   puts $fo $tableToDisplay
   close $fo
 }

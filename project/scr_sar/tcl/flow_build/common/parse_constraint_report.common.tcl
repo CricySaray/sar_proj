a1bebf206d7b0ff4394e55a175fa50a5a6385926
cricy <cricysaray@qq.com>
2025-09-29 03:02:33 +0800

    update runCmd_summarize_pt_rpts.pt.tcl

--- project/scr_sar/tcl/flow_build/common/parse_constraint_report.common.tcl
+++ project/scr_sar/tcl/flow_build/common/parse_constraint_report.common.tcl
@@ -8,10 +8,53 @@
 # descrip   : Parse the report_constraint command to obtain the number of VIOLATED instances and the maximum violated value for constraints such as setup, 
 #             hold, max_transition, max_cap, max_fanout, min_pulse_width, and min_period.
 # return    : nested list: {{min_delay worstViolValue violNum} {max_daley worstViolValue violNum} {max_capacitance worstViolValue violNum} {max_transition worstViolValue violNum} {max_fanout worstViolValue violNum} {min_pulse_width worstViolValue violNum} {min_period worstViolValue violNum}}
-# NOTICE    : please generate report file using command: report_constraint -all_violator -path_type end|slack_only -nos -max_delay -min_delay -max_transition -max_capacitance -max_fanout -min_period -min_pulse_width
+# NOTICE    : please generate report file using command: report_constraint -all_violator -path_type slack_only -nos -max_delay -min_delay -max_transition -max_capacitance -max_fanout -min_period -min_pulse_width
+#             waive_list_file: split with blank character
+#             line 1: CAP /inst/to/pin/name
+#             line 2: SETUP /inst/to/pin/name
+#             line 3: HOLD /inst/to/pin/name
+#             line 4: HOLD /inst/to/pin/name
+#             line 5: TRAN /inst/to/pin/name
+#             line 6: CAP /inst/to/pin/name
+#             line 7: CAP regexp:{^\w+/U_PAD_ADDA_\w+/PAD$}
+#             line 8: TRAN regexp:{^U_ANARF_TOP/\w+$}
+#             line 9: CAP /inst/to/pin/name
+#             NOTICE: The first item of each sublist in `waive_prefix_prompt_list` must be one of the following options. It is recommended to set the corresponding `waive_prefix_prompt` for all 
+#             the following items (which means this list will have a total of 7 items). Alternatively, you can set only a few of them. If your `waive_list_file` does not include the `waive_pin` 
+#             settings for some of these items, you may skip setting the `waive_prefix_prompt` for those specific types.
+#               "max_delay min_delay max_transition max_capacitance max_fanout min_pulse_width min_period"
 # ref       : link url
 # --------------------------
-proc parse_constraint_report {filename {debug 0}} {
+source ../../packages/every_any.package.tcl; # every
+source ../../packages/print_formattedTable.package.tcl; # print_formattedTable
+proc parse_constraint_report {filename {waive_list_file ""} {waive_prefix_prompt_list {{max_delay SETUP} {min_delay HOLD} {max_transition TRAN} {max_capacitance CAP} {max_fanout FANOUT} {min_pulse_width PULSE} {min_period PERIOD}}} {outputDir "./"} {prefixOutPutFile "sor_DesignName_"} {debug 0}} {
+  if {![llength $waive_prefix_prompt_list] || $waive_list_file == ""} {
+    set flagToWaive 0
+  } else {
+    set flagToWaive 1
+  }
+  if {$waive_list_file != "" && ![file isfile $waive_list_file]} {
+    error "proc parse_constraint_report: check your input : waive_list_file($waive_list_file) is not found!!!"
+  } elseif {$waive_list_file != "" && [file isfile $waive_list_file] && ![llength $waive_list_file]} {
+    error "proc parse_constraint_report: check your input : waive_list_file($waive_list_file) exists but waive_prefix_prompt_list($waive_prefix_prompt_list) is empty!!!"
+  }
+  set fi [open $waive_list_file r] ; set waive_content [split [read $fi] "\n"] ; close $fi ; set waive_content [lsearch -not -all -inline -regexp $waive_content {^\s*$}]
+  if {![every x $waive_content {expr {[llength $x] == 2}}]} {
+    error "proc parse_constraint_report: check your input: waive_list_file($waive_list_file) have line that is not only include two columns!!! check it."
+  }
+  set all_prefix_prompt_on_waive_file [lsort -u [lmap temp_line $waive_content { lindex $temp_line 0 }]]
+  set all_avaiable_prefix_prompt [lmap temp_list $waive_prefix_prompt_list { lindex $temp_list 1 }]
+  if {![every x $all_prefix_prompt_on_waive_file { expr {$x in $all_avaiable_prefix_prompt} }]} {
+    set invalid_lines [lmap temp_line $waive_content {
+      if {[lindex $temp_line 0] ni $all_avaiable_prefix_prompt} {
+        set temp_error $temp_line
+      } else {
+        continue
+      }
+    }]
+    set prefix_prompt_invalid [lsort -u [lmap temp_line $invalid_lines { lindex $temp_line 0 }]]
+    error "proc parse_constraint_report: check your input: waive_list_file($waive_list_file) have invalid prefix prompt: $prefix_prompt_invalid\ninvalid lines:\n[join $invalid_lines \n]"
+  }
   set max_delay_content "NA"
   set min_delay_content "NA"
   set max_capacitance_content "NA"
@@ -91,6 +134,9 @@ proc parse_constraint_report {filename {debug 0}} {
       set path $path_val
       if {$debug} {puts "Found -path option with value: $path_val"}
     }
+  }
+  if {$path eq "slack_only"} {
+    error "proc parse_constraint_report: check your command of report_constraint, please use option: -path end , dont use -path slack_only"
   }
   
   set blocks [list]
@@ -242,32 +288,49 @@ proc parse_constraint_report {filename {debug 0}} {
   return [_prepare_result \
     $max_delay_content $min_delay_content \
     $max_capacitance_content $max_transition_content $max_fanout_content \
-    $min_pulse_width_content $min_period_content]
+    $min_pulse_width_content $min_period_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
 }
 
 proc _prepare_result {max_delay_content min_delay_content \
   max_capacitance_content max_transition_content max_fanout_content \
-  min_pulse_width_content min_period_content} {
+  min_pulse_width_content min_period_content waive_list_file waive_prefix_prompt_list outputDir prefixOutPutFile} {
   
   set result [list]
   
-  lappend result [_process_constraint "min_delay" $min_delay_content]
-  lappend result [_process_constraint "max_delay" $max_delay_content]
-  lappend result [_process_constraint "max_capacitance" $max_capacitance_content]
-  lappend result [_process_constraint "max_transition" $max_transition_content]
-  lappend result [_process_constraint "max_fanout" $max_fanout_content]
-  lappend result [_process_constraint "min_pulse_width" $min_pulse_width_content]
-  lappend result [_process_constraint "min_period" $min_period_content]
+  lappend result [_process_constraint "min_delay" $min_delay_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
+  lappend result [_process_constraint "max_delay" $max_delay_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
+  lappend result [_process_constraint "max_capacitance" $max_capacitance_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
+  lappend result [_process_constraint "max_transition" $max_transition_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
+  lappend result [_process_constraint "max_fanout" $max_fanout_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
+  lappend result [_process_constraint "min_pulse_width" $min_pulse_width_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
+  lappend result [_process_constraint "min_period" $min_period_content $waive_list_file $waive_prefix_prompt_list $outputDir $prefixOutPutFile]
   
   return $result
 }
 
-proc _process_constraint {type content} {
+proc _process_constraint {type content waive_list_file waive_prefix_prompt_list outputDir prefixOutPutFile} {
+  switch $type {
+    "max_delay" {set outputFileBody "setup"}
+    "min_delay" {set outputFileBody "hold"}
+    default {set outputFileBody $type}
+  }
   if {$content eq "NA"} {
+    set output_file "${outputDir}/${prefixOutPutFile}$outputFileBody.viol.list"
+    if {[file isdirectory [file dirname $output_file]]} {
+      exec touch $output_file
+    } else {
+      error "proc _process_constraint: check your outputDir($outputDir) is not found!!!(NA mode)"
+    }
     return [list $type "NA" "NA"]
   }
   
   if {$content eq "EMPTY"} {
+    set output_file "${outputDir}/${prefixOutPutFile}$outputFileBody.viol.list"
+    if {[file isdirectory [file dirname $output_file]]} {
+      exec touch $output_file
+    } else {
+      error "proc _process_constraint: check your outputDir($outputDir) is not found!!!(EMPTY mode)"
+    }
     return [list $type 0 0]
   }
   
@@ -288,21 +351,30 @@ proc _process_constraint {type content} {
   set processed [list]
   foreach line $filtered {
     # Use regex to extract pin and value directly from line
-    if {[regexp {\s*(\S+)\s+.*?(\S+)\s+\(VIOLATED} $line -> pin value]} {
+    if {[regexp {\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+\(VIOLATED} $line -> pin required_path_delay actual_path_delay value]} {
       if {[string is double -strict $value]} {
-        lappend processed [list $pin $value]
+        lappend processed [list $pin $required_path_delay $actual_path_delay $value]
       }
     }
   }
-  
-  set num [llength $processed]
+  set removed_waive_item_precessed [_remove_waive_items $type $processed $waive_list_file $waive_prefix_prompt_list]
+  set list_to_dump_rpt [linsert $removed_waive_item_precessed 0 [list viol_pin required_path_delay actual_path_delay slack_value]]
+  set output_file "${outputDir}/${prefixOutPutFile}$outputFileBody.viol.list"
+  if {[file isdirectory [file dirname $output_file]]} {
+    set fo [open $output_file w]
+    puts $fo [print_formattedTable $list_to_dump_rpt] ; close $fo
+  } else {
+    error "proc _process_constraint: check your outputDir($outputDir) is not found!!!"
+  }
+
+  set num [llength $removed_waive_item_precessed]
   if {$num == 0} {
     return [list $type 0 0]
   }
   
   set min_val Inf
-  foreach item $processed {
-    lassign $item pin val
+  foreach item $removed_waive_item_precessed {
+    lassign $item pin require actual val
     if {$val < $min_val} {
       set min_val $val
     }
@@ -310,4 +382,37 @@ proc _process_constraint {type content} {
   
   return [list $type $min_val $num]
 }
-
+proc _remove_waive_items {type content_list waive_list_file waive_prefix_prompt_list} {
+  if {![llength $content_list]} {
+    return $content_list
+  } else {
+    if {![llength $waive_prefix_prompt_list] || $waive_list_file == ""} {
+      return $content_list
+    } else {
+      set fi [open $waive_list_file r] ; set waive_content [split [read $fi] "\n"] ; close $fi ; set waive_content [lsearch -not -all -inline -regexp $waive_content {^\s*$}]
+      if {![every x $waive_content {expr {[llength $x] == 2}}]} {
+        error "proc _remove_waive_items: check your input : waive_list_file($waive_list_file) have lines that is not only include two columns!!! check it."
+      } else {
+        if {$type ni {max_delay min_delay max_transition max_capacitance max_fanout min_pulse_width min_period}} {
+          error "proc _remove_waive_items: check your input : type($type) is not one of (max_delay min_delay max_transition max_capacitance max_fanout min_pulse_width min_period) !!!"
+        }
+        set prefix_prompt_of_type [lindex [lsearch -index 0 -exact -inline $waive_prefix_prompt_list $type] 1]
+        set waive_pin_of_type [lmap temp_line $waive_content {
+          if {[lindex $temp_line 0] eq $prefix_prompt_of_type} {
+            set temp [lindex $temp_line 1]
+          } else {
+            continue
+          }
+        }]
+        foreach temp_pin_expr $waive_pin_of_type {
+          if {[regexp {^regexp:(.*)} $temp_pin_expr -> temp_regExp]} {
+            set content_list [lsearch -regexp -index 0 -not -all -inline $content_list $temp_regExp]
+          } else {
+            set content_list [lsearch -exact -index 0 -not -all -inline $content_list $temp_pin_expr]
+          }
+        }
+        return $content_list
+      }
+    }
+  }
+}

da3ef3677d9f11218acfd50f7addf8fbce56f46a
cricy <cricysaray@qq.com>
2025-09-29 19:55:07 +0800

    Auto commit at 2025-09-29 19:55

--- project/scr_sar/tcl/flow_build/FM/runCmd_formalityFlow.fm.tcl
+++ project/scr_sar/tcl/flow_build/FM/runCmd_formalityFlow.fm.tcl
@@ -10,11 +10,13 @@
 # ref       : link url
 # --------------------------
 proc runCmd_formalityFlow {args} {
-  set designName       ""
-  set dbList           "" ; # can be any rc corner db list
-  set referenceNetlist ""
-  set implementNetlist ""
-  set logFileName      ""
+  set designName                 ""
+  set dbList                     "" ; # can be any rc corner db list
+  set referenceNetlist           ""
+  set implementNetlist           ""
+  set logFileName                ""
+  set failingPointReportFileName ""
+  set constantSettingCmdsList    [list]
   parse_proc_arguments -args $args opt
   foreach arg [array names opt] {
     regsub -- "-" $arg "" var
@@ -26,12 +28,35 @@ proc runCmd_formalityFlow {args} {
     if {$dbList == ""} {
       error "proc runCmd_formalityFlow: check your input: dbList($dbList) is empty!!!" 
     } else {
-      if {![file isdirectory [file dirname $logFileName]]} {
-        error "proc runCmd_formalityFlow: check your input: dirname([file dirname $logFileName]) of logFileName($logFileName) is not a directory!!!"
+      if {![file isdirectory [file dirname $logFileName]] || ![file isdirectory [file dirname $failingPointReportFileName]]} {
+        error "proc runCmd_formalityFlow: check your input: dirname([file dirname $logFileName]) of logFileName($logFileName) or dirname([file dirname $failingPointReportFileName]) of failingPointReportFileName($failingPointReportFileName) is not a directory!!!"
       } else {
         set hdlin_unresolved_modules black_box
         set hdlin_warn_on_mismatch_message
         set hdlin_warn_on_mismatch_message "FMR_ELAB-147 FMR_ELAB-146" 
+        set synopsys_auto_setup true
+        set verification_clock_gate_edge_analysis true
+        set verification_failing_point_limit 1000
+        set_host_options -max_cores 8
+        set DESIGN $designName
+
+        # --- Library Variable Setting
+        set_app_var search_path ". "
+        set link_library $dbList
+        read_db $link_library
+        read_verilog -r $referenceNetlist
+        set_top r:/WORK/$DESIGN
+        read_verilog -i $implementNetlist
+        set_top i:/WORK/$DESIGN
+        if {$constantSettingCmdsList != ""} {
+          foreach temp_constant $constantSettingCmdsList {
+            eval $temp_constant 
+          }
+        }
+        report_black_boxes -all
+        match
+        verify
+        report_failing_point > $failingPointReportFileName
       }
     }
   }

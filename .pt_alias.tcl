fc4692b3efad9d7a11ee000267a3c45fec098896
cricy <cricysaray@qq.com>
2025-09-30 19:05:01 +0800

    Auto commit at 2025-09-30 19:05

--- .pt_alias.tcl
+++ .pt_alias.tcl
@@ -1,6 +1,7 @@
 # ------------------------
 # pt alias 
 
+alias s "source"
 alias sa "source ~/.pt_alias.tcl"
 alias rs "restore_session"
 alias sc "set_host_options -max_cores" ; # you need input number of max cores

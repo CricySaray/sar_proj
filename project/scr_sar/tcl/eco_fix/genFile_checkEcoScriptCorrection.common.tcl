42aef9e4a1d470daa8bea435feb85917dec1905c
cricy <cricysaray@qq.com>
2025-10-02 22:00:06 +0800

    Auto commit at 2025-10-02 22:00

---
+++ project/scr_sar/tcl/eco_fix/genFile_checkEcoScriptCorrection.common.tcl
@@ -0,0 +1,13 @@
+#!/bin/tclsh
+# --------------------------
+# author    : sar song
+# date      : 2025/10/02 21:50:27 Thursday
+# label     : flow_proc
+#   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc|flow_proc|report_proc|cross_lang_proc|misc_proc)
+#   perl -> (format_sub|getInfo_sub|perl_task)
+# descrip   : Check the correctness of all commands in the eco script. The main checks include verifying whether the commands used are compliant and whether they meet the 
+#             requirements of each option within the commands. For example, regarding option names: when using `addRepeater`, the names specified by `-name` must be unique. 
+#             When inserting an `inverter`, if a name needs to be specified, it must be enclosed in double curly braces like `{{}}`, and so on.  
+# return    : 
+# ref       : link url
+# --------------------------

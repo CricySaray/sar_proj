d03bc729cc2a4526e1cfcfe8b3dab746d6d7c770
cricy <cricysaray@qq.com>
2025-10-11 16:50:03 +0800

    update perl : cp_files.pl

--- project/scr_sar/perl/cp_files.pl
+++ project/scr_sar/perl/cp_files.pl
@@ -1,4 +1,14 @@
 #!/usr/bin/perl
+# --------------------------
+# author    : sar song
+# date      : 2025/10/11 16:49:11 Saturday
+# label     : perl_task
+#   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc|flow_proc|report_proc|cross_lang_proc|eco_proc|misc_proc)
+#   perl -> (format_sub|getInfo_sub|perl_task)
+# descrip   : Copies files from multiple source directories to a single target directory, adding a path component as prefix or suffix to filenames to prevent overwriting.
+# return    : /
+# ref       : link url
+# --------------------------
 use strict;
 use warnings;
 use File::Copy;

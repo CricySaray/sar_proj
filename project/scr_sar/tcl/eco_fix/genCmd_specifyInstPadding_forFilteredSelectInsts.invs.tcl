b46e901386e7d8b32c152c884ddbb0710de541fd
cricy <cricysaray@qq.com>
2025-10-17 20:10:02 +0800

    Auto commit at 2025-10-17 20:10

---
+++ project/scr_sar/tcl/eco_fix/genCmd_specifyInstPadding_forFilteredSelectInsts.invs.tcl
@@ -0,0 +1,12 @@
+#!/bin/tclsh
+# --------------------------
+# author    : sar song
+# date      : 2025/10/17 20:09:45 Friday
+# label     : 
+#   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc
+#             |flow_proc|report_proc|cross_lang_proc|eco_proc|misc_proc)
+#   perl -> (format_sub|getInfo_sub|perl_task|flow_perl)
+# descrip   : what?
+# return    : 
+# ref       : link url
+# --------------------------

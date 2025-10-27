ed00131fdca6317b4cdb354c90033eca60f4d18c
cricy <cricysaray@qq.com>
2025-10-27 21:20:05 +0800

    Auto commit at 2025-10-27 21:20

--- project/scr_sar/tcl/eco_fix/forPV/genCmd_fixVTL_P_orVTL_N.invs.tcl
+++ project/scr_sar/tcl/eco_fix/forPV/genCmd_fixVTL_P_orVTL_N.invs.tcl
@@ -12,32 +12,38 @@
 # --------------------------
 proc genCmd_fixVTL_P_orVTL_N {args} {
   set markersNameList    [list VTL_N.S.1 VTL_P.S.1]
-  set prefixOfToCelltype "LVT"
+  set suffixOfToCelltype "LVT"
   parse_proc_arguments -args $args opt
   foreach arg [array names opt] {
     regsub -- "-" $arg "" var
     set $var $opt($arg)
   }
+  set all_boxes [list]
   set boxes [lmap temp_marker $markersNameList {
     set temp_box [dbget [dbget top.markers.userType $temp_marker -p].box]
+    lappend all_boxes {*}$temp_box
   }]
-  set insts [dbget [dbQuery -areas $boxes -objType inst -enclose_only].name]
+  set insts [dbget [dbQuery -areas $all_boxes -objType inst -enclosed_only].name]
   if {$insts eq ""} {
     error "proc genCmd_fixVTL_P_orVTL_N: have no inst to get when markers == $markersNameList  !!!" 
   } else {
     set cmdsList [lmap temp_inst $insts {
       set temp_celltype [dbget [dbget top.insts.name $temp_inst -p].cell.name -e]
       if {[regexp {BWP$} $temp_celltype]} {
-        set temp_celltype [string cat $temp_celltype $prefixOfToCelltype] 
+        set temp_celltype [string cat $temp_celltype $suffixOfToCelltype] 
       }
+      set temp_cmd "ecoChangeCell -cell $temp_celltype -inst $temp_inst"
     }] 
+    set cmdsList [linsert $cmdsList 0 "setEcoMode -batchMode true -updateTiming false -refinePlace false -honorDontTouch false -honorDontUse false -honorFixedNetWire false -honorFixedStatus false"]
+    set cmdsList [linsert $cmdsList 0 "setEcoMode -reset"]
+    set cmdsList [linsert $cmdsList end "setEcoMode -reset"]
+    return $cmdsList
   }
 }
 
 define_proc_arguments genCmd_fixVTL_P_orVTL_N \
-  -info "whatFunction"\
+  -info "gen cmd for fixing VTL_P.S.1 or VTL_N.S.1"\
   -define_args {
-    {-type "specify the type of eco" oneOfString one_of_string {required value_type {values {change add delRepeater delNet move}}}}
-    {-inst "specify inst to eco when type is add/delete" AString string require}
-    {-distance "specify the distance of movement of inst when type is 'move'" AFloat float optional}
+    {-markersNameList "specify the list of markers name" AList list optional}
+    {-suffixOfToCelltype "specify suffix of toCelltype" AString string optional}
   }

135d6036e6205511acc766858c68520cd5a6820b
cricy <cricysaray@qq.com>
2026-01-04 18:15:01 +0800

    Auto commit at 2026-01-04 18:15

---
+++ project/scr_sar/tcl/flow_build/invs_flow/runCmd_addRoutingBlockage_forCoreToDieArea.invs.tcl
@@ -0,0 +1,30 @@
+#!/bin/tclsh
+# --------------------------
+# author    : sar song
+# date      : 2026/01/04 18:12:08 Sunday
+# label     : 
+#   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc
+#             |flow_proc|report_proc|cross_lang_proc|eco_proc|misc_proc)
+#   perl -> (format_sub|getInfo_sub|perl_task|flow_perl)
+# descrip   : what?
+# return    : 
+# ref       : link url
+# --------------------------
+proc runCmd_addRoutingBlockage_forCoreToDieArea {args} {
+  set nameOfRoutingBlockage "boundary_rblkg"
+  parse_proc_arguments -args $args opt
+  foreach arg [array names opt] {
+    regsub -- "-" $arg "" var
+    set $var $opt($arg)
+  }
+  catch {deleteRouteBlk -name $nameOfRoutingBlockage}
+  set pin_lists [dbget top.terms.name]
+  
+}
+define_proc_arguments PROC_NAME \
+  -info "whatFunction"\
+  -define_args {
+    {-type "specify the type of eco" oneOfString one_of_string {required value_type {values {change add delRepeater delNet move}}}}
+    {-inst "specify inst to eco when type is add/delete" AString string require}
+    {-distance "specify the distance of movement of inst when type is 'move'" AFloat float optional}
+  }

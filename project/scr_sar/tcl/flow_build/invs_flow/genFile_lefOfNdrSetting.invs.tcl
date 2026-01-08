cfa0ac14ae45b95a254f872731ddeb67619f7e42
cricy <cricysaray@qq.com>
2026-01-08 20:43:13 +0800

    add proc genFile_lefOfNdrSetting.invs.tcl

---
+++ project/scr_sar/tcl/flow_build/invs_flow/genFile_lefOfNdrSetting.invs.tcl
@@ -0,0 +1,32 @@
+#!/bin/tclsh
+# --------------------------
+# author    : sar song
+# date      : 2026/01/08 20:41:04 Thursday
+# label     : 
+#   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc
+#             |flow_proc|report_proc|cross_lang_proc|eco_proc|misc_proc|snippet)
+#   perl -> (format_sub|getInfo_sub|perl_task|flow_perl)
+# descrip   : what?
+# return    : 
+# ref       : link url
+# --------------------------
+# TO_WRITE
+proc genFile_lefOfNdrSetting {args} {
+  set ruleOfWide "M1:0.1 M2:0.1 "
+  set ruleOfSpacing "M1:0.1 M2:0.1"
+  parse_proc_arguments -args $args opt
+  foreach arg [array names opt] {
+    regsub -- "-" $arg "" var
+    set $var $opt($arg)
+  }
+  
+  
+}
+
+define_proc_arguments genFile_lefOfNdrSetting \
+  -info "whatFunction"\
+  -define_args {
+    {-type "specify the type of eco" oneOfString one_of_string {required value_type {values {change add delRepeater delNet move}}}}
+    {-inst "specify inst to eco when type is add/delete" AString string require}
+    {-distance "specify the distance of movement of inst when type is 'move'" AFloat float optional}
+  }

29ff5744983fc4f0420f222e69e53b3345cdba7b
cricy <cricysaray@qq.com>
2026-01-27 18:30:01 +0800

    Auto commit at 2026-01-27 18:30

--- project/scr_sar/tcl/eco_fix/check_pushClockTree_ifCorrect_forEvenNumberInverter.invs.tcl
+++ project/scr_sar/tcl/eco_fix/check_pushClockTree_ifCorrect_forEvenNumberInverter.invs.tcl
@@ -6,7 +6,9 @@
 #   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc
 #             |flow_proc|report_proc|cross_lang_proc|eco_proc|misc_proc|snippet|signoff_check)
 #   perl -> (format_sub|getInfo_sub|perl_task|flow_perl)
-# descrip   : what?
+# descrip   : Please use the format of 'attachTerm instname cellpinname [get_object_name [get_nets -of instpinname]]' to write valid 
+#             commands. This can also prevent situations where net names change in some ECOs.
+#
 # return    : 
 # ref       : link url
 # --------------------------
@@ -23,8 +25,9 @@ proc check_pushClockTree_ifCorrect_forEvenNumberInverter {args} {
   set fi [open $inputFilename]
   set content [split [read $fi] \n]
   close $fi
-  set attachTermsCmdList [lsearch -index 0 -all -inline $content attachTerm]
+  set attachTermsCmdList [lsearch -regexp -all -inline $content {^\s*attachTerm}]
   puts [join $attachTermsCmdList \n]
+  
 }
 define_proc_arguments check_pushClockTree_ifCorrect_forEvenNumberInverter \
   -info "whatFunction"\

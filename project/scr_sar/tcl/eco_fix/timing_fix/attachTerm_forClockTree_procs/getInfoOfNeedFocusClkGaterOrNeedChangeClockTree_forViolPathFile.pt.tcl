5848cbc0e11df8ea12a834742e52d08034b86e41
cricy <cricysaray@qq.com>
2026-02-16 22:50:03 +0800

    Auto commit at 2026-02-16 22:50

--- project/scr_sar/tcl/eco_fix/timing_fix/attachTerm_forClockTree_procs/getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile.pt.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/attachTerm_forClockTree_procs/getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile.pt.tcl
@@ -11,7 +11,33 @@ proc getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile {args} {
     regsub -- "-" $arg "" var
     set $var $opt($arg)
   }
-  
+  if {![file exists $violPathFile]} {
+    error "proc getInfoOfFluencyOfAttachedTerm_forNewViolPath: input error, not found viol file: $violPathFile" 
+  }
+  exec mkdir -p ./$tmp_dir_name
+  exec grep -E "Startpoint:|Endpoint:" $violPathFile > $tmp_dir_name/simple_newViolFile.rpt
+  set fi [open $tmp_dir_name/simple_newViolFile.rpt r]
+  set flagSearchStartOrEndpoint "start"
+  set pathOfStartToEndList [list]
+  set temp_start_end_pair [list]
+  while {[gets $fi line] != -1} {
+    if {$flagSearchStartOrEndpoint eq "start"} {
+      if {[regexp "Startpoint:" $line]} { lappend temp_start_end_pair [lindex $line end] ; set flagSearchStartOrEndpoint "end" } else {
+        error "proc getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile: error viol path content, can find startpoint-endpoint pair when find startpoint at line:\n\t$line" 
+      }
+    } elseif {$flagSearchStartOrEndpoint eq "end"} {
+      if {[regexp "Endpoint:" $line]} { lappend temp_start_end_pair [lindex $line end] ; set flagSearchStartOrEndpoint "start" } else {
+        error "proc getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile: error viol path content, can find startpoint-endpoint pair when find endpoint at line:\n\t$line" 
+      }
+      if {[llength $temp_start_end_pair] == 2} {
+        lappend pathOfStartToEndList $temp_start_end_pair
+        set temp_start_end_pair [list] 
+      }
+    }
+  }
+  close $fi
+  set pathNum [llength $pathOfStartToEndList]
+  puts "total find $pathNum path."
   
 }
 

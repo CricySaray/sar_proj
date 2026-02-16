bdb6836c1c76c40d888a579ece96c98495a73407
cricy <cricysaray@qq.com>
2026-02-17 00:18:47 +0800

    add new proc for clock tree process: getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile.pt.tcl

--- project/scr_sar/tcl/eco_fix/timing_fix/attachTerm_forClockTree_procs/getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile.pt.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/attachTerm_forClockTree_procs/getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile.pt.tcl
@@ -1,11 +1,15 @@
 proc getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile {args} {
   # NOTICE: need find points after common point
-  set violPathFile            ""
-  set numNeedContinueCkBufInv 4
-  set typeOfPathClockTree     "launch" ; # launch|capture
-  set tmp_dir_name            ".tmp_dir_for_get_simple_viol_path_file"
-  set output_dir              "./"
-  set outputFileBodyName      "findSameClockTreePart"
+  set violPathFile                  ""
+  set numNeedContinueCkBufInv       4
+  set pba_mode                      "ex"
+  set buffOrInvRegExp               {DCCKN|DCCKB|CKB|CKN}
+  set clkgaterCelltypeRegExp        {^CKLNQD}
+  set keepContinueClockTreeInstName {U_MAIN_SUB}
+  set typeOfPathClockTree           "launch" ; # launch|capture
+  set tmp_dir_name                  ".tmp_dir_for_get_simple_viol_path_file"
+  set output_dir                    "./"
+  set outputFileBodyName            "findSameClockTreePart"
   parse_proc_arguments -args $args opt
   foreach arg [array names opt] {
     regsub -- "-" $arg "" var
@@ -39,12 +43,127 @@ proc getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile {args} {
   set pathNum [llength $pathOfStartToEndList]
   puts "total find $pathNum path."
   
+  set clockTreeMeetConditionPathBlock [list]
+  set notFindMeetContinueBufInvPathList [list]
+  foreach temp_path $pathOfStartToEndList {
+    lassign $temp_path temp_start temp_end 
+    set temp_col_full_clock_path [get_timing_paths -pba_mode $pba_mode -path_type full_clock_expanded -from $temp_start -to $temp_end ]
+    set temp_path_slack [get_attribute $temp_col_full_clock_path slack]
+    if {$typeOfPathClockTree eq "launch"} {
+      set temp_col_launch_clock_points [get_attribute [get_attribute [get_attribute $temp_col_full_clock_path launch_clock_paths] points] object]
+      set temp_list_launch_clock_points [get_object_name $temp_col_launch_clock_points]
+      set temp_list_launch_clock_insts [lreverse [lsort -u [get_object_name [get_attribute $temp_col_launch_clock_points cell]]]]
+      if {$keepContinueClockTreeInstName ne ""} {
+        set temp_list_launch_clock_insts_after_process_continue_keep_condition [list] 
+        set flagOfBeginMonitor 0
+        foreach temp_inst $temp_list_launch_clock_insts {
+          if {[regexp $keepContinueClockTreeInstName $temp_inst] && !$flagOfBeginMonitor} {
+            set flagOfBeginMonitor 1 
+            lappend temp_list_launch_clock_insts_after_process_continue_keep_condition $temp_inst
+          }
+          if {![regexp $keepContinueClockTreeInstName $temp_inst] && $flagOfBeginMonitor} {
+            break 
+          }
+        }
+      }
+      set temp_num_of_buf_or_inv 0
+      set temp_num_of_clkgater 0
+      set temp_save_buffer_or_inverter_name [list] ; # order: from prev inst to after inst
+      foreach temp_inst $temp_list_launch_clock_insts_after_process_continue_keep_condition {
+        if {[regexp -expanded $buffOrInvRegExp [get_attribute [get_cells $temp_inst] ref_name]]} {
+          incr temp_num_of_buf_or_inv 
+          set temp_save_buffer_or_inverter_name [linsert $temp_save_buffer_or_inverter_name 0 $temp_inst]
+          if {$temp_num_of_buf_or_inv >= $numNeedContinueCkBufInv} {
+            lappend clockTreeMeetConditionPathBlock [list $temp_path_slack $temp_path $temp_save_buffer_or_inverter_name ] 
+            set temp_save_buffer_or_inverter_name [list]
+            set temp_num_of_buf_or_inv 0
+          }
+        } else {
+          set temp_num_of_buf_or_inv 0 
+        }
+        if {$temp_inst eq [lindex $temp_list_launch_clock_insts_after_process_continue_keep_condition end]} {
+          lappend notFindMeetContinueBufInvPathList [list $temp_path_slack $temp_path]
+        }
+      }
+      
+    } elseif {$typeOfPathClockTree eq "capture"} {
+      set temp_col_capture_clock_points [get_attribute [get_attribute [get_attribute $temp_col_full_clock_path capture_clock_paths] points] object]
+      set temp_list_capture_clock_points [get_object_name $temp_col_capture_clock_points]
+      set temp_list_capture_clock_insts [lreverse [lsort -u [get_object_name [get_attribute $temp_col_capture_clock_points cell]]]]
+      if {$keepContinueClockTreeInstName ne ""} {
+        set temp_list_capture_clock_insts_after_process_continue_keep_condition [list] 
+        set flagOfBeginMonitor 0
+        foreach temp_inst $temp_list_capture_clock_insts {
+          if {[regexp $keepContinueClockTreeInstName $temp_inst] && !$flagOfBeginMonitor} {
+            set flagOfBeginMonitor 1 
+            lappend temp_list_capture_clock_insts_after_process_continue_keep_condition $temp_inst
+          }
+          if {![regexp $keepContinueClockTreeInstName $temp_inst] && $flagOfBeginMonitor} {
+            break 
+          }
+        }
+      }
+      set temp_num_of_buf_or_inv 0
+      set temp_num_of_clkgater 0
+      set temp_save_buffer_or_inverter_name [list] ; # order: from prev inst to after inst
+      foreach temp_inst $temp_list_capture_clock_insts_after_process_continue_keep_condition {
+        if {[regexp -expanded $buffOrInvRegExp [get_attribute [get_cells $temp_inst] ref_name]]} {
+          incr temp_num_of_buf_or_inv 
+          set temp_save_buffer_or_inverter_name [linsert $temp_save_buffer_or_inverter_name 0 $temp_inst]
+          if {$temp_num_of_buf_or_inv == $numNeedContinueCkBufInv} {
+            lappend clockTreeMeetConditionPathBlock [list $temp_path_slack $temp_path $temp_save_buffer_or_inverter_name ] 
+            set temp_save_buffer_or_inverter_name [list]
+            set temp_num_of_buf_or_inv 0
+          }
+        } else {
+          set temp_num_of_buf_or_inv 0 
+        }
+        if {$temp_inst eq [lindex $temp_list_capture_clock_insts_after_process_continue_keep_condition end]} {
+          lappend notFindMeetContinueBufInvPathList [list $temp_path_slack $temp_path]
+        }
+      }
+    } else {
+      error "proc getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile: error typeOfPathClockTree(need launch or capture): $typeOfPathClockTree" 
+    }
+  }
+  set finalClockClockPath_to_violPath [list]
+  foreach temp_clock_tree_info_block $clockTreeMeetConditionPathBlock {
+    lassign $temp_clock_tree_info_block temp_path_slack temp_path temp_save_buffer_or_inverter_name 
+    if {$finalClockClockPath_to_violPath eq "" || [lsearch -index 0 $finalClockClockPath_to_violPath $temp_save_buffer_or_inverter_name] == -1} {
+      lappend finalClockClockPath_to_violPath [list $temp_save_buffer_or_inverter_name [list [list $temp_path_slack $temp_path]]] 
+    } elseif {$finalClockClockPath_to_violPath ne "" && [lsearch -index 0 $finalClockClockPath_to_violPath $temp_save_buffer_or_inverter_name] != -1} {
+      lset finalClockClockPath_to_violPath [lsearch -index 0 $finalClockClockPath_to_violPath $temp_save_buffer_or_inverter_name] 1 [list {*}[lindex $finalClockClockPath_to_violPath [lsearch -index 0 $finalClockClockPath_to_violPath $temp_save_buffer_or_inverter_name] 1] [list $temp_path_slack $temp_path]]
+    }
+  }
+  set outputfilename "$output_dir/$outputFileBodyName.rpt"
+  set fo [open $outputfilename w]
+  foreach temp_block_info $finalClockClockPath_to_violPath {
+    lassign $temp_block_info temp_save_buffer_or_inverter_name temp_paths 
+    puts $fo "No 1:"
+    puts $fo "continue buffer or inverter inst:"
+    puts $fo [join $temp_save_buffer_or_inverter_name \n]
+    puts $fo "RELATED PATHS: (slack startpoint endpoint)"
+    foreach temp_path $temp_paths {
+      lassign $temp_path temp_slack temp_start temp_end
+      puts $fo "$temp_slack $temp_start\n\t$temp_end"
+    }
+    puts ""
+  }
+  close $fo
+  
 }
 
 define_proc_attributes getInfoOfNeedFocusClkGaterOrNeedChangeClockTree_forViolPathFile \
   -info "get info of need focus clk gater or need change clock tree for viol path file"\
   -define_args {
-    {-type "specify the type of eco" oneOfString one_of_string {optional value_type {values {change add delRepeater delNet move}}}}
-    {-inst "specify inst to eco when type is add/delete" AString string optional}
-    {-distance "specify the distance of movement of inst when type is 'move'" AFloat float optional}
+    {-violPathFile "specify the viol path file name" AString string optional}
+    {-numNeedContinueCkBufInv "specify the num of need continue clock buffer or inverter" AString string optional}
+    {-pba_mode "specify the pba mode for get_timing_paths" AString string optional}
+    {-buffOrInvRegExp "specify expression of buffer or inverter" AString string optional}
+    {-clkgaterCelltypeRegExp "specify the clkgater celltype regexpression" AString string optional}
+    {-keepContinueClockTreeInstName "specify the keep continue clock tree inst name" AString string optional}
+    {-typeOfPathClockTree "specify the type of path clock tree to find" AString string optional}
+    {-tmp_dir_name "specify the tmp dir name" AString string optional}
+    {-output_dir "specify the output dir" AString string optional}
+    {-outputFileBodyName "specify the output file body name" AString string optional}
   }

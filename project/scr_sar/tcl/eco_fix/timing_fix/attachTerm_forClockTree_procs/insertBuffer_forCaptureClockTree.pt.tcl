0b2808b73cc22fc54fb24f3559f240e4cc03fcfc
cricy <cricysaray@qq.com>
2026-02-21 11:48:13 +0800

    add proc insertBuffer_forCaptureClockTree.pt.tcl

--- project/scr_sar/tcl/eco_fix/timing_fix/attachTerm_forClockTree_procs/insertBuffer_forCaptureClockTree.pt.tcl
+++ project/scr_sar/tcl/eco_fix/timing_fix/attachTerm_forClockTree_procs/insertBuffer_forCaptureClockTree.pt.tcl
@@ -1,21 +1,18 @@
-#!/bin/tclsh
-# --------------------------
 # author    : sar song
 # date      : 2026/02/20 22:08:19 Friday
 # label     : clock_tree_relative_proc
 #   tcl  -> (atomic_proc|display_proc|gui_proc|task_proc|dump_proc|check_proc|math_proc|package_proc|test_proc|datatype_proc|db_proc
 #             |flow_proc|report_proc|cross_lang_proc|eco_proc|misc_proc|snippet|signoff_check|drc_proc|clock_tree_relative_proc)
 #   perl -> (format_sub|getInfo_sub|perl_task|flow_perl)
-# descrip   : This is set to facilitate the insertion of multiple repeaters, and can automatically increment the iterative number in the suffix of the cell name.
+# descrip   : This is set to facilitate the insertion of multiple repeaters, and can automatically increment the iterative number in the suffix of the cell name.  for pt
 # return    : run eco cmd
 # ref       : link url
 # --------------------------
-proc insertBuffer_forCaptureClockTree {args} {
+proc insertBuffer_forCaptureClockTree_pt {args} {
   set suffixForEco                "fix_mem2reg"
   set numOfInsert                 1
   set celltypeOfBufferToInsert    DCCKBD4BWP35P140LVT
   set terms                       {}
-  set location                    {}
   set ifDryRun                    1
   set ifResetIterationCounter     0
   set initIndexOfIterationCounter 1 ; # nornally is 0/1, you can set other number as the initial number of iteration counter
@@ -27,12 +24,12 @@ proc insertBuffer_forCaptureClockTree {args} {
   global song_eco_counter
   if {$ifResetIterationCounter} {
     if {![array exists song_eco_counter]} {
-      puts "proc insertBuffer_forCaptureClockTree: not need reset, cuz it have been reset."
+      puts "proc insertBuffer_forCaptureClockTree_pt: not need reset, cuz it have been reset."
     } elseif {![catch {array unset song_eco_counter}]} {
-      puts "proc insertBuffer_forCaptureClockTree: reset iteration counter SUCCESS!!!"
+      puts "proc insertBuffer_forCaptureClockTree_pt: reset iteration counter SUCCESS!!!"
       return [list]
     } else {
-      error "proc insertBuffer_forCaptureClockTree: error when unset array song_eco_counter!!! check your code"
+      error "proc insertBuffer_forCaptureClockTree_pt: error when unset array song_eco_counter!!! check your code"
     }
   }
 
@@ -42,27 +39,21 @@ proc insertBuffer_forCaptureClockTree {args} {
     # incr song_eco_counter(eco_num)
   }
   if {$terms eq ""} {
-    error "proc insertBuffer_forCaptureClockTree: check your input , terms is empty!!!"
+    error "proc insertBuffer_forCaptureClockTree_pt: check your input , terms is empty!!!"
   } else {
     foreach temp_term $terms {
-      if {[dbget top.insts.instTerms.name $temp_term -e] eq ""} {
-        error "proc insertBuffer_forCaptureClockTree: not found term name ($temp_term)"
+      if {[get_pins -q $temp_term] eq ""} {
+        error "proc insertBuffer_forCaptureClockTree_pt: not found term name ($temp_term)"
       }
     }
-    if {[dbget head.libCells.name $celltypeOfBufferToInsert -e] eq ""} {
-      error "proc insertBuffer_forCaptureClockTree: check your input : not found celltype of buffer/inverter($celltypeOfBufferToInsert) in invs library."
+    if {[get_lib_cells -q */$celltypeOfBufferToInsert] eq ""} {
+      error "proc insertBuffer_forCaptureClockTree_pt: check your input : not found celltype of buffer/inverter($celltypeOfBufferToInsert) in invs library."
     }
     set finalCmdsList [list]
     for {set i 1} {$i <= $numOfInsert} {incr i} {
-      if {$location eq ""} {
-        incr song_eco_counter(eco_num)
-        set temp_cmd "ecoAddRepeater -name ${suffixForEco}_${song_eco_counter(eco_num)} -cell $celltypeOfBufferToInsert -term \{$terms\}"
-        lappend finalCmdsList $temp_cmd
-      } else {
-        incr song_eco_counter(eco_num)
-        set temp_cmd "ecoAddRepeater -name ${suffixForEco}_${song_eco_counter(eco_num)} -cell $celltypeOfBufferToInsert -term \{$terms\} -loc \{$location\}"
-        lappend finalCmdsList $temp_cmd
-      }
+      incr song_eco_counter(eco_num)
+      set temp_cmd "insert_buffer \{$terms\} $celltypeOfBufferToInsert -new_cell_name newcell_${suffixForEco}_${song_eco_counter(eco_num)} -new_net_names newnet_${suffixForEco}_${song_eco_counter(eco_num)}"
+      lappend finalCmdsList $temp_cmd
     }
     if {$ifDryRun} {
       foreach temp_final_cmd $finalCmdsList {
@@ -77,15 +68,14 @@ proc insertBuffer_forCaptureClockTree {args} {
     }
   }
 }
-define_proc_arguments insertBuffer_forCaptureClockTree \
+define_proc_attributes insertBuffer_forCaptureClockTree_pt \
   -info "insert buffer for capture clock tree"\
   -define_args {
-    {-ifDryRun "if only print cmd but not run actually" oneOfString one_of_string {optional value_type {values {0 1}}}}
+    {-ifDryRun "if only print cmd but not run actually" oneOfString one_of_string {optional value_help {values {0 1}}}}
     {-suffixForEco "specify the eco suffix name" AString string optional}
     {-numOfInsert "specify the num of buffer to insert" AInt int optional}
     {-celltypeOfBufferToInsert "celltype of buffer to insert" AString string optional}
     {-terms "specify the term(s) to run ecoAddRepeater" AList list optional}
-    {-location "specify the location of new repeater" AList list optional}
-    {-ifResetIterationCounter "if reset iteration counter" oneOfString one_of_string {optional value_type {values {0 1}}}}
+    {-ifResetIterationCounter "if reset iteration counter" oneOfString one_of_string {optional value_help {values {0 1}}}}
     {-initIndexOfIterationCounter "specify the initial index of iteration counter" AInt int optional}
   }
